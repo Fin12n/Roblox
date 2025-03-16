17a2013dc4c26575a8db966597f5199aa419b26d
Fin12n <thinhminecraftpe1@gmail.com>
2025-03-17 01:08:51 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,10 @@
+local CoreGui = game:GetService("StarterGui")
+
+if game.PlaceId == 12716055617 then
+  CoreGui:SetCore("SendNotification", {
+	Title = "Notification";
+	Text = "Is this a notification?";
+	Duration = 5;
+})
+    loadtring
+end
