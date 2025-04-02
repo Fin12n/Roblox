bebd869daa2a8b5ff907272ccfa9677e9fdde94b
Fin12n <thinhminecraftpe1@gmail.com>
2025-04-02 10:31:12 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,19 +1,22 @@
-local NotifyLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Fin12n/Roblox/refs/heads/main/UI-Library/NotificationLib.lua"))
 local LoadingLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Fin12n/Roblox/refs/heads/main/UI-Library/Loading.lua"))()
 
-if game.PlaceId == "12716055617" then 
+if game.PlaceId == 12716055617 then
     LoadingLib:CreateLoading({
         Title = "FINN HUB",
         Image = "rbxassetid://137595879440980", -- Thay bằng ID hình ảnh hợp lệ
         ScriptName = "Emergency Emden Menu"
     })
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fin12n/Roblox/refs/heads/main/Script/Emden.lua"))
-    NotifyLib:Notification({
-        Title = "Game Detected ❗",
-        Content = "Emergency Emden",
-        Image = "137595879440980",
-        Duration = 5
-    })
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fin12n/Roblox/refs/heads/main/Script/Emden.lua", true))()
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Game Detected ❗";
+    Text = "Emergency Emden"; -- what the text says (ofc)
+    Duration = 10;
+})
+game.StarterGui:SetCore("SendNotification", {
+    Title = "By Fjnnn From Finnn Hub";
+    Text = "Have Fun!"; -- what the text says (ofc)
+    Duration = 15;
+})
 end
 if game.PlaceId == "286090429" then 
     LoadingLib:CreateLoading({
@@ -22,10 +25,14 @@ if game.PlaceId == "286090429" then
         ScriptName = "Arsenal Menu"
     })
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Fin12n/Roblox/refs/heads/main/Script/Arsenal.lua"))
-    NotifyLib:Notification({
-        Title = "Game Detected ❗",
-        Content = "Arsenal",
-        Image = "137595879440980",
-        Duration = 5
-    })
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Game Detected ❗";
+    Text = "Arsenal"; -- what the text says (ofc)
+    Duration = 10;
+})
+game.StarterGui:SetCore("SendNotification", {
+    Title = "By Fjnnn From Finnn Hub";
+    Text = "Have Fun!"; -- what the text says (ofc)
+    Duration = 15;
+})
 end
