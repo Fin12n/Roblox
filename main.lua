5b2f11a1eb0c2054e2aa4a19811a096772ce6684
Fin12n <thinhminecraftpe1@gmail.com>
2025-04-02 10:58:37 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,14 +2,32 @@ local LoadingLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Fi
 
 if game.PlaceId == 12716055617 then
     LoadingLib:CreateLoading({
-        Title = "FINN HUB",
+        Title = "Finnn HUB",
         Image = "rbxassetid://137595879440980", -- Thay bằng ID hình ảnh hợp lệ
         ScriptName = "Emergency Emden Menu"
     })
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fin12n/Roblox/refs/heads/main/Script/Emden.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fin12n/Roblox/refs/heads/main/Script/Emden.lua"))()
 game.StarterGui:SetCore("SendNotification", {
     Title = "Game Detected ❗";
     Text = "Emergency Emden"; -- what the text says (ofc)
+    Duration = 10;
+})
+game.StarterGui:SetCore("SendNotification", {
+    Title = "By Fjnnn From Finnn Hub";
+    Text = "Have Fun!"; -- what the text says (ofc)
+    Duration = 15;
+})
+end
+if game.PlaceId == 286090429 then
+    LoadingLib:CreateLoading({
+        Title = "Finnn HUB",
+        Image = "rbxassetid://79901055258586", -- Thay bằng ID hình ảnh hợp lệ
+        ScriptName = "Arsenal Menu"
+    })
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fin12n/Roblox/refs/heads/main/Script/Emden.lua"))()
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Game Detected ❗";
+    Text = "Arsenal"; -- what the text says (ofc)
     Duration = 10;
 })
 game.StarterGui:SetCore("SendNotification", {
